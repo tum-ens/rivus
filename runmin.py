eb5b06dabda045f228b0c825ab5e904a4b7c5ced
Johannes Dorfner <johannes.dorfner@tum.de>
2013-09-19 15:25:41 +0200

    ADD: input DataFrames now available after runmin.py

--- runmin.py
+++ runmin.py
@@ -1,13 +1,16 @@
 import capmin
 import pandashp as pdshp
 import pandaspyomo as pdpo
+import pyomotools
 from coopr.opt.base import SolverFactory
+from operator import itemgetter
 
 building_shapefile = 'data/mnl/mnl_building'
 edge_shapefile = 'data/mnl/mnl_edge'
 vertex_shapefile = 'data/mnl/mnl_vertex'
 data_spreadsheet = 'data/mnl/mnl.xlsx'
 
+solver_name = 'glpk' # possible (if licensed and binary in path): cplex, gurobi 
 
 # load buildings and sum by type and nearest edge ID
 # 1. read shapefile to DataFrame (with special geometry column)
@@ -22,20 +25,26 @@ total_area = buildings_grouped.sum()['total_area'].unstack()
 # 1. read shapefile to DataFrame (with geometry column)
 # 2. join DataFrame total_area on index (=ID)
 # 3. fill missing values with 0
-edges = pdshp.read_shp(edge_shapefile)
-edges_w_area = edges.join(total_area)
-edges_w_area = edges_w_area.fillna(0)
+edge = pdshp.read_shp(edge_shapefile)
+edge = edge.join(total_area)
+edge = edge.fillna(0)
 
 # load nodes
-nodes = pdshp.read_shp(vertex_shapefile)
+vertex = pdshp.read_shp(vertex_shapefile)
 
 # create model
-model = capmin.create_model(data_spreadsheet, nodes, edges_w_area)
+model = capmin.create_model(data_spreadsheet, vertex, edge)
 instance = model.create()
-solver = SolverFactory('glpk')
+solver = SolverFactory(solver_name)
 
 # solve problem
-#result = solver.solve(instance)
-#instance.load(result)
+result = solver.solve(instance)
+instance.load(result)
 
 instance.write()
+
+# prepare input data similar to model for easier analysis
+ig=itemgetter('Commodity', 'Process', 'Process-Commodity', 'Time', 'Area-Demand')
+dfs = pyomotools.read_xls(data_spreadsheet)
+commodity, process, process_commodity, time, area_demand = ig(dfs)
+
