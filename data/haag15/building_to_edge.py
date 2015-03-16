e87c78d85bc9e907eca2680b5d1a0df4bcf7b105
Johannes Dorfner <johannes.dorfner@tum.de>
2015-03-16 14:19:34 +0100

    mod: haag15/building_to_edge uses geopandas for geographic coord support

--- data/haag15/building_to_edge.py
+++ data/haag15/building_to_edge.py
@@ -1,3 +1,4 @@
+import geopandas
 import pandas as pd
 import pandashp as pdshp
 import shutil
@@ -11,13 +12,24 @@ to_edge_filename = 'to_edge'
 blds_w_near_filename = 'building_w_nearest'
 
 # DO
-buildings = pdshp.read_shp(buildings_filename)
-edge = pdshp.read_shp(edge_filename)
+# read
+buildings = geopandas.read_file(buildings_filename+'.shp')
+edge = geopandas.read_file(edge_filename+'.shp')
+
+# project to UTM zone 33N (=EPSG:32600 + zone number for north)
+buildings = buildings.to_crs(epsg=32633)
+edge = edge.to_crs(epsg=32633)
+
+# find closest edge
 to_edge = pdshp.find_closest_edge(buildings, edge, to_attr='Edge')
+to_edge = geopandas.GeoDataFrame(to_edge)
+to_edge.crs = edge.crs.copy()
+
+# reproject back to geographic WGS 84 (EPSG:4326)
+buildings = buildings.to_crs(epsg=4326)
+edge = edge.to_crs(epsg=4326)
+to_edge = to_edge.to_crs(epsg=4326)
 
 # FINISH
-pdshp.write_shp(to_edge_filename, to_edge)
-pdshp.write_shp(blds_w_near_filename, buildings, write_index=False)
-
-shutil.copyfile(edge_filename+'.prj', to_edge_filename+'.prj')
-shutil.copyfile(buildings_filename+'.prj', blds_w_near_filename+'.prj') 
+to_edge.to_file(to_edge_filename+'.shp')
+buildings.to_file(blds_w_near_filename+'.shp')
