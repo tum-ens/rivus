5278ef1deb2c7d2d709d3236ca41d862f5e06b78
lnksz <kristof.havasi@tum.de>
2017-07-10 14:58:16 +0200

    Integrated the nitpicking corrections, formatted docstrings to numpy style

--- rivus/gridder/__init__.py
+++ rivus/gridder/__init__.py
@@ -1,5 +1,5 @@
 # See: https://docs.python.org/3/tutorial/modules.html#packages
-# Create more inteligent way of importting module components
+# Create more intelligent way of importing module components
 #__all__ = ["create_square_grid", "surround", "reverse"]
 from .create_grid import create_square_grid
 from .extend_grid import extend_edge_data, vert_init_commodities
--- rivus/gridder/extend_grid.py
+++ rivus/gridder/extend_grid.py
@@ -3,76 +3,94 @@ if __name__ == '__main__':
     from shapely.geometry import Point
 
 
-# Add Commodity columns filled with to the vertex DataFrame,
 def vert_init_commodities(vertex_df, commodities, sources=None):
-    """Add commodity columns with zeros to vertex DataFrame
-    source: (Commodity, Index, Value) tuples/lists
-        e.g. ('Elec', 1, 2000)
+    """Add commodity columns to the vertex DataFrame
+    with zeros to vertices without commodity source and
+    source capacity at the vertices provided by `sources`.
 
-    Args:
-        vertex_df ((Geo)DataFrame): vertex dataframe input
-        commodities (lis of str): Like ('Elec', 'Gas', 'Heat')
-        sources (list of tuples, optional): Init the source nodes
-            Like [('Elec', 0, 100000), ('Gas', 1, 50000)]
+    Parameters
+    ----------
+    vertex_df : (Geo)DataFrame
+        vertex dataframe input
+    commodities : list of str
+        Like ('Elec', 'Gas', 'Heat')
+    sources : list of tuples, optional
+        Init the source nodes.
+        Tuple form:(Commodity, Index, Value)
+
+    Raises
+    ------
+    ValueError
+    If parameters differ from awaited
+
+    Usage
+    -----
+    comms = ('Elec', 'Gas')
+    sources = [('Elec', 0, 1000), ('Gas', 1, 500)]
+    vert_init_commodities(vert, comms, sources)
     """
     for commo in commodities:
         vertex_df[commo] = len(vertex_df) * [0]
     if sources:
         for s in sources:
-            check1 = (len(s) == 3) and isinstance(s[0], str)
-            check2 = isinstance(s[1], int) and isinstance(s[2], (int, float)) and \
-                s[1] < len(vertex_df)
-            if check1 and check2:
+            is_well_typed = (isinstance(s[0], str) and isinstance(s[1], int)
+                             and isinstance(s[2], (int, float)))
+            is_good_dims = (len(s) == 3) and s[1] < len(vertex_df)
+            if is_well_typed and is_good_dims:
                 vertex_df.set_value(index=s[1], col=s[0], value=s[2])
             else:
-                raise ValueError('Parameter problem in function call. ch1: {}, ch2: {}'.format(check1, check2))
+                raise ValueError('Parameter problem in function call. ' +
+                                 'ch1: {} ch2: {}'.format(check1, check2))
 
 
-def extend_edge_data(edge_df, dtype='area', strat='equal', sorts=None, inits=None):
-    """Add various data to the edge (Geo)DataFrame
-        Normally used to add demand data
-    
+def extend_edge_data(edge_df, sorts=None, inits=None, strat='equal',
+                     strat_param=None):
+    """Add demand data to the edges in a (Geo)DataFrame
+
     Parameters
     ----------
     edge_df : (Geo)DataFrame
         edge dataframe to be extended
-    dtype : str, optional
-        Type of data whit wich we extend (todo)
-    strat : str, optional
-        How the data values will be created
     sorts : list of str, optional
         The names of new columns (extensions)
+        Defaults to ['residential']
     inits : list of int/float , optional
-        The parameter values,
-        matching to sorts argument.
-    
+        The parameter values, matching to sorts argument.
+        Defaults to [1000] for each sort.
+    strat : str, optional [TODO now only 'equal' has an effect]
+        How the data values will be created
+        + 'equal' - all edge demand is the same
+        + 'linear' - linearly decreasing
+        + 'exp' - exponentially decreasing
+        + 'manual' - provide mapper in strat_param
+    strat_param : optional [TODO no implemented yet]
+        Parameter for linear | exp | manual strategies.
+        + 'equal' - None - no effect
+        + 'linear' - minimum (lowest demand)
+        + 'exp' - minimum (lowest demand)
+        + 'manual' - function/dict to fetch value per edge
+
     Raises
     ------
     ValueError
-    If input is not like awaited
+    If inputs differ from awaited
+
+    Usage
+    -----
+    sorts = ('residential', 'other')
+    inits = (1000, 800)
+    extend_edge_data(edge, sorts=sorts, inits=inits)
     """
 
-    # TODO data validation
-    # sorts-initvals
-
     # How the data will be distributed among the edges
-    # equal: equally, each edge has same value(s)
-    # linear: TBD
-    # exp: TBD
-    # manual: could be cool for plug-in strategy / mask
     strat = 'equal' if strat not in [
-        'equal', 'linear', 'exp', 'manual'] else strat
-
-    # area: as used in the haag project
-    # demand: could be used after rivus can take direct demand values
-    #         could be proportional to edge length, or place in the grid?
-    dtypes = ['area', 'damand']
+        'equal', 'linear', 'exp'] else strat
 
     # Value to the distribution, and types
     sorts = ['residential'] if not sorts else sorts
     inits = [1000] * len(sorts) if not inits else inits
     if len(sorts) != len(inits):
-        raise ValueError('sorts and initvals are not equal long')
+        raise ValueError('sorts and initvals do not have same length.')
 
     if strat == 'equal':
         for idx, sort in enumerate(sorts):
