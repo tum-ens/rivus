2ded688c4ff2b78c3f9c8b1f2e52c01123045a30
lnksz <kristof.havasi@tum.de>
2017-07-26 19:02:26 +0200

    Fix weight handling for connectedness analysis

--- rivus/graph/to_graph.py
+++ rivus/graph/to_graph.py
@@ -52,15 +52,17 @@ def to_igraph(vdf, edf, pmax, comms=None, peak=None, save_dir=None, ext='gml'):
                 'lgr', 'ncol', 'net', 'pajek', 'pickle', 'picklez', 'svg']
     if ext not in ext_list:
         ext = 'gml'
-    if len(edf) != len(pmax):
-        pmax = edf.join(pmax).fillna(0)
+
     comms = pmax.columns.values if comms is None else comms
+    if len(edf) != len(pmax):
+        # To have all rows, also the ones, where nothing was built.
+        pmax = edf.join(pmax).fillna(0)
 
     graphs = []
     for comm in comms:
         # Graph can be created from vertices, because no isolated node is
         # in the input graph.
-        g = ig.Graph(edf.index.values.tolist())
+        g = ig.Graph(pmax[comm].index.values.tolist())
         g['Name'] = '{} capacity graph'.format(comm.upper())
         g['Commodity'] = comm
         g.vs['Label'] = vdf.index.values.tolist()
@@ -68,13 +70,21 @@ def to_igraph(vdf, edf, pmax, comms=None, peak=None, save_dir=None, ext='gml'):
         g.es['Label'] = list(map(lambda v1v2: '{}-{}'.format(*v1v2),
                                  edf.index.values))
         g.es[comm] = pmax[comm].tolist()
-        weights = pmax[comm] / pmax[comm].max()
-        g.es['Weight'] = weights.tolist()
+        cap_max = pmax[comm].max()
+        if cap_max == 0:
+            weights = [0] * len(pmax)
+        else:
+            weights = pmax[comm] / cap_max
+        g.es['Weight'] = weights.tolist()  # Camel case for Gephi
+        g.es['weight'] = weights.tolist()
         if peak is not None:
             g.es[comm + '-peak'] = peak[comm].tolist()
+
         # For possible GeoLayout (e.g. in Gephi)
         g.vs['Longitude'] = vdf.geometry.map(lambda p: p.x).tolist()
         g.vs['Latitude'] = vdf.geometry.map(lambda p: p.y).tolist()
+        # Delete demand aedges, what are not built in optimum
+        g.delete_edges(g.es.select(weight_eq=0))
         graphs.append(g)
 
     if save_dir:
@@ -113,11 +123,12 @@ def to_nx(vdf, edf, pmax, comms=None, save_dir=None):
         slower and does not improve code quality in my opinion.
     """
     import networkx as nx
+    comms = pmax.columns.values if comms is None else comms
     if len(edf) != len(pmax):
+        # To have all rows, also the ones, where nothing was built.
         _pmax = edf.join(pmax).fillna(0)
     else:
         _pmax = pmax.copy()
-    comms = _pmax.columns.values if comms is None else comms
 
     graphs = []
     for comm in comms:
@@ -130,11 +141,16 @@ def to_nx(vdf, edf, pmax, comms=None, save_dir=None):
             g.node[x]['Longitude'] = row.geometry.x
             g.node[x]['Latitude'] = row.geometry.y
         cap_max = _pmax[comm].max()
-        for v1v2, row in _pmax.iterrows():
-            this_weight = row[comm] / cap_max
-            this_label = '{}-{}'.format(*v1v2)
-            g.add_edge(*v1v2, Label=this_label, Commodity=row[comm],
-                       Weight=this_weight)
+        if cap_max != 0:
+            for v1v2, row in _pmax.iterrows():
+                if row[comm] == 0:
+                    # do not connect verticies in the graph,
+                    # if the edge is not built in optimum
+                    continue
+                this_weight = row[comm] / cap_max
+                this_label = '{}-{}'.format(*v1v2)
+                g.add_edge(*v1v2, Label=this_label, Commodity=row[comm],
+                           Weight=this_weight)
         graphs.append(g)
 
     if save_dir:
