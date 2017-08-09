601fc81fcedbe379ac163163cae369b360a3b95a
lnksz <kristof.havasi@tum.de>
2017-08-10 01:42:27 +0200

    Adapted to not yet merged fixed from branch gridder

--- rivus/gridder/__init__.py
+++ rivus/gridder/__init__.py
@@ -1,2 +1,6 @@
 # See: https://docs.python.org/3/tutorial/modules.html#packages
-#__all__ = ["create_square_grid", "surround", "reverse"]
+# Create more intelligent way of importing module components
+from . import create_grid
+from . import extend_grid
+
+['__all__ = ["create_square_grid","extend_edge_data","vert_init_commodities"]']
