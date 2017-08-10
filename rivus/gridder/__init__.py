63b3bd3df327cfd85b4a51a6347e54d6b8b6b575
lnksz <kristof.havasi@tum.de>
2017-08-10 18:37:49 +0200

    Uncheck gridder changes to ease merge into current main.

--- rivus/gridder/__init__.py
+++ rivus/gridder/__init__.py
@@ -1,6 +1,2 @@
 # See: https://docs.python.org/3/tutorial/modules.html#packages
-# Create more intelligent way of importing module components
-from . import create_grid
-from . import extend_grid
-
-['__all__ = ["create_square_grid","extend_edge_data","vert_init_commodities"]']
+#__all__ = ["create_square_grid", "surround", "reverse"]
