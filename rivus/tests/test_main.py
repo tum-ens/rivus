2eaf43801a05bc80dbbe957725ecd24823dd0144
lnksz <kristof.havasi@tum.de>
2017-08-27 15:35:51 +0200

    Add the sub package for unittests
    The files serve mainly as skeletons, but some actual tests are already added.

---
+++ rivus/tests/test_main.py
@@ -0,0 +1,27 @@
+import unittest
+from rivus.main.rivus import read_excel
+
+
+class RivusTest(unittest.TestCase):
+
+    # TODO There is plenty of more functions (if not all)
+    # which reside in the code-base untested. Bellow some placeholders.
+
+    def test_line_length(self):
+        pass
+
+    def test_source_calculation(self):
+        pass
+
+    def test_pair_vertex_to_edge(self):
+        pass
+
+    def test_save_load(self):
+        pass
+
+    def test_read_excel(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
